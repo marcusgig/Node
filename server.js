516a080eeb47e00a9a201d963e664a54a64178e6
marcusgig <marcusviniciusaf.dev@gmail.com>
2026-03-24 22:14:40 -0300

    Subindo todas as alteracoes pro git

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
 import express from 'express';
+import cors from 'cors';
+import { PrismaClient } from './prisma/generated/client/index.js';
+const prisma = new PrismaClient();
 
 
 const app = express();
 app.use(express.json())
-const users = []
+app.use(cors())
 
-app.get("/usuarios/:id", (req, res) => {
-    console.log(req)
-   res.status(200).json({users})
+
+app.get("/usuarios/:id", async (req, res) => {
+    const users = await prisma.user.findmany()
+    res.status(200).json({users})
 
 });
 
-app.post("/usuarios", (req, res) => {
-    users.push(req.body)
-    res.status(201).json({message: "Usuario criado com sucesso!"})
+app.post("/usuarios", async (req, res) => {
+ const user = await   prisma.user.create({
+    data:{
+        name: req.body.name,
+        age: req.body.age,
+        email: req.body.email
+    }
+ });
+    res.status(201).json({user})
+    
 });
 
 app.listen(3000)
