330bf32638790ddbebc6e8ea24870e4da3837207
marcusgig <marcusviniciusaf.dev@gmail.com>
2026-03-29 19:56:41 -0300

    Aplicacao Finalizada

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.use(express.json())
 app.use(cors())
 
 
-app.get("/usuarios/:id", async (req, res) => {
-    const users = await prisma.user.findmany()
-    res.status(200).json({users})
-
+app.get("/usuarios", async (req, res) => {
+  
+    const users = await prisma.user.findMany() 
+    res.status(200).json(users)
 });
 
 app.post("/usuarios", async (req, res) => {
@@ -26,5 +26,15 @@ app.post("/usuarios", async (req, res) => {
     res.status(201).json({user})
     
 });
+
+app.delete('/usuarios/:id', async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        },
+    });
+    res.status(200).json({ message: "Usuário deletado com sucesso!" });
+});
+
 
 app.listen(3000)
